e9ae38b9c049b1167daa6709923bc76fc26d8c98
sonali13399 <sitalsonali@gmail.com>
2020-09-07 12:46:51 +0530

    Create anagrams.cpp

---
+++ anagrams.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+
+#include <string.h>
+
+
+ 
+
+using namespace std;
+
+
+ 
+
+int main(){
+
+    int t;
+
+    cin >> t;
+
+    string s1, s2;
+
+    int i;
+
+    while (t--){
+
+int count = 0;
+
+        cin >> s1 >> s2;
+
+
+ 
+
+        int fc[26] = {0}, fc2[26] = {0};
+
+
+ 
+
+        for (i = (int)(s1.length()); i > 0 ; i--){
+
+            fc[(int)(s1[i-1])-97]++;
+
+        }
+
+
+ 
+
+        for (i = (int)(s2.length()); i > 0 ; i--){
+
+            fc2[(int)(s2[i-1])-97]++;
+
+        }
+
+
+ 
+
+        for(i = 0; i<26; i++){
+
+            if (fc[i] > fc2[i]){ count = count + (fc[i]-fc2[i]);}
+
+else if (fc2[i] > fc[i]) { count = count + (fc2[i]-fc[i]); }
+
+        }
+
+
+
+
+ 
+
+cout << count<<endl;
+
+count = 0;
+
+    }
+
+    return 0;
+
+}
+
